abcbbef5a82b9499569ed8eedab2faaf9ebeed91
Erich Ess <erich.g.ess@gmail.com>
2020-05-21 14:49:12 -0400

    Formatting

--- src/math/matrix.rs
+++ src/math/matrix.rs
@@ -1,18 +1,16 @@
-use std::f32::*;
-use super::{Vector3, Vector4};
 use super::point::Point3;
+use super::{Vector3, Vector4};
+use std::f32::*;
 
 /// Row Major matrix
 #[derive(Debug, PartialEq, Copy, Clone)]
 pub struct Matrix {
-    mat: [[f32;4];4],
+    mat: [[f32; 4]; 4],
 }
 
 impl Matrix {
     pub fn new() -> Matrix {
-        Matrix {
-            mat: [[0.;4];4],
-        }
+        Matrix { mat: [[0.; 4]; 4] }
     }
 
     pub fn get(&self, row: usize, col: usize) -> f32 {
@@ -20,11 +18,13 @@ impl Matrix {
     }
 
     pub fn identity() -> Matrix {
-        Matrix{
-            mat: [[1., 0., 0., 0.],
-                  [0., 1., 0., 0.],
-                  [0., 0., 1., 0.],
-                  [0., 0., 0., 1.]],
+        Matrix {
+            mat: [
+                [1., 0., 0., 0.],
+                [0., 1., 0., 0.],
+                [0., 0., 1., 0.],
+                [0., 0., 0., 1.],
+            ],
         }
     }
 
@@ -57,20 +57,20 @@ impl Matrix {
     pub fn mat_mul(&self, a: &Matrix) -> Matrix {
         let s = &self.mat;
         let a = &a.mat;
-        let help = move|row:usize, col:usize| {
+        let help = move |row: usize, col: usize| {
             let mut sum = 0.;
             for i in 0..4 {
-                sum += s[row][i]*a[i][col];
+                sum += s[row][i] * a[i][col];
             }
             sum
         };
-        Matrix{
-            mat:[
-                [help(0,0), help(0,1), help(0,2), help(0,3)],
-                [help(1,0), help(1,1), help(1,2), help(1,3)],
-                [help(2,0), help(2,1), help(2,2), help(2,3)],
-                [help(3,0), help(3,1), help(3,2), help(3,3)],
-            ]
+        Matrix {
+            mat: [
+                [help(0, 0), help(0, 1), help(0, 2), help(0, 3)],
+                [help(1, 0), help(1, 1), help(1, 2), help(1, 3)],
+                [help(2, 0), help(2, 1), help(2, 2), help(2, 3)],
+                [help(3, 0), help(3, 1), help(3, 2), help(3, 3)],
+            ],
         }
     }
 
@@ -98,10 +98,13 @@ impl Matrix {
             if self.mat[col][col].abs() < EPSILON {
                 let mut big_row = col;
                 for row in 0..4 {
-                    if self.mat[row][col].abs() > self.mat[big_row][col].abs() { big_row = row; }
+                    if self.mat[row][col].abs() > self.mat[big_row][col].abs() {
+                        big_row = row;
+                    }
                 }
-                if big_row == col {panic!("Singular Matrix");}
-                else {
+                if big_row == col {
+                    panic!("Singular Matrix");
+                } else {
                     for j in 0..4 {
                         let tmp = self.mat[big_row][j];
                         self.mat[big_row][j] = self.mat[col][j];
@@ -139,81 +142,112 @@ impl Matrix {
     }
 
     pub fn scale(x: f32, y: f32, z: f32) -> Matrix {
-        Matrix{
-            mat: [[x, 0., 0., 0.],
-                  [0., y, 0., 0.],
-                  [0., 0., z, 0.],
-                  [0., 0., 0., 1.]],
+        Matrix {
+            mat: [
+                [x, 0., 0., 0.],
+                [0., y, 0., 0.],
+                [0., 0., z, 0.],
+                [0., 0., 0., 1.],
+            ],
         }
     }
 
     pub fn translate(x: f32, y: f32, z: f32) -> Matrix {
-        Matrix{
-            mat: [[1., 0., 0., x],
-                  [0., 1., 0., y],
-                  [0., 0., 1., z],
-                  [0., 0., 0., 1.]],
+        Matrix {
+            mat: [
+                [1., 0., 0., x],
+                [0., 1., 0., y],
+                [0., 0., 1., z],
+                [0., 0., 0., 1.],
+            ],
         }
     }
 
     pub fn rotate_x(angle: f32) -> Matrix {
         let pi = consts::PI;
         let rads = angle / 180.0 * pi;
-        
-        Matrix{
-            mat: [[1., 0., 0., 0.],
-                  [0., rads.cos(), -rads.sin(), 0.],
-                  [0., rads.sin(), rads.cos(), 0.],
-                  [0., 0., 0., 1.]],
+
+        Matrix {
+            mat: [
+                [1., 0., 0., 0.],
+                [0., rads.cos(), -rads.sin(), 0.],
+                [0., rads.sin(), rads.cos(), 0.],
+                [0., 0., 0., 1.],
+            ],
         }
     }
 
     pub fn rotate_y(angle: f32) -> Matrix {
         let pi = consts::PI;
         let rads = angle / 180.0 * pi;
-        
-        Matrix{
-            mat: [[rads.cos(), 0., rads.sin(), 0.],
-                  [0., 1., 0., 0.],
-                  [-rads.sin(), 0., rads.cos(), 0.],
-                  [0., 0., 0., 1.]],
+
+        Matrix {
+            mat: [
+                [rads.cos(), 0., rads.sin(), 0.],
+                [0., 1., 0., 0.],
+                [-rads.sin(), 0., rads.cos(), 0.],
+                [0., 0., 0., 1.],
+            ],
         }
     }
 
     pub fn rotate_z(angle: f32) -> Matrix {
         let pi = consts::PI;
         let rads = angle / 180.0 * pi;
-        
-        Matrix{
-            mat: [[rads.cos(), -rads.sin(), 0., 0.],
-                  [rads.sin(), rads.cos(), 0., 0.],
-                  [0., 0., 1., 0.],
-                  [0., 0., 0., 1.]],
+
+        Matrix {
+            mat: [
+                [rads.cos(), -rads.sin(), 0., 0.],
+                [rads.sin(), rads.cos(), 0., 0.],
+                [0., 0., 1., 0.],
+                [0., 0., 0., 1.],
+            ],
         }
     }
 
     pub fn vec_mul(&self, v: &Vector4) -> Vector4 {
         Vector4::new(
-            v.x()*self.get(0,0) + v.y()*self.get(0,1) + v.z()*self.get(0,2) + v.w()*self.get(0,3),
-            v.x()*self.get(1,0) + v.y()*self.get(1,1) + v.z()*self.get(1,2) + v.w()*self.get(1,3),
-            v.x()*self.get(2,0) + v.y()*self.get(2,1) + v.z()*self.get(2,2) + v.w()*self.get(2,3),
-            v.x()*self.get(3,0) + v.y()*self.get(3,1) + v.z()*self.get(3,2) + v.w()*self.get(3,3),
-                )
+            v.x() * self.get(0, 0)
+                + v.y() * self.get(0, 1)
+                + v.z() * self.get(0, 2)
+                + v.w() * self.get(0, 3),
+            v.x() * self.get(1, 0)
+                + v.y() * self.get(1, 1)
+                + v.z() * self.get(1, 2)
+                + v.w() * self.get(1, 3),
+            v.x() * self.get(2, 0)
+                + v.y() * self.get(2, 1)
+                + v.z() * self.get(2, 2)
+                + v.w() * self.get(2, 3),
+            v.x() * self.get(3, 0)
+                + v.y() * self.get(3, 1)
+                + v.z() * self.get(3, 2)
+                + v.w() * self.get(3, 3),
+        )
     }
 
     pub fn vec3_mul(&self, v: &Vector3) -> Vector3 {
         Vector3::new(
-            v.x()*self.get(0,0) + v.y()*self.get(0,1) + v.z()*self.get(0,2),
-            v.x()*self.get(1,0) + v.y()*self.get(1,1) + v.z()*self.get(1,2),
-            v.x()*self.get(2,0) + v.y()*self.get(2,1) + v.z()*self.get(2,2),
+            v.x() * self.get(0, 0) + v.y() * self.get(0, 1) + v.z() * self.get(0, 2),
+            v.x() * self.get(1, 0) + v.y() * self.get(1, 1) + v.z() * self.get(1, 2),
+            v.x() * self.get(2, 0) + v.y() * self.get(2, 1) + v.z() * self.get(2, 2),
         )
     }
 
     pub fn pt_mul(&self, p: &Point3) -> Point3 {
         Point3::new(
-            p.x()*self.get(0,0) + p.y()*self.get(0,1) + p.z()*self.get(0,2) + self.get(0,3),
-            p.x()*self.get(1,0) + p.y()*self.get(1,1) + p.z()*self.get(1,2) + self.get(1,3),
-            p.x()*self.get(2,0) + p.y()*self.get(2,1) + p.z()*self.get(2,2) + self.get(2,3),
+            p.x() * self.get(0, 0)
+                + p.y() * self.get(0, 1)
+                + p.z() * self.get(0, 2)
+                + self.get(0, 3),
+            p.x() * self.get(1, 0)
+                + p.y() * self.get(1, 1)
+                + p.z() * self.get(1, 2)
+                + self.get(1, 3),
+            p.x() * self.get(2, 0)
+                + p.y() * self.get(2, 1)
+                + p.z() * self.get(2, 2)
+                + self.get(2, 3),
         )
     }
 }
@@ -234,7 +268,11 @@ mod tests {
         for row in 0..4 {
             for col in 0..4 {
                 let eq = (a.mat[row][col] - b.mat[row][col]).abs() < eps;
-                assert!(eq, "[{}][{}] unequal.  left is {} and right is {}", row, col, a.mat[row][col], b.mat[row][col]);
+                assert!(
+                    eq,
+                    "[{}][{}] unequal.  left is {} and right is {}",
+                    row, col, a.mat[row][col], b.mat[row][col]
+                );
             }
         }
     }
@@ -262,17 +300,20 @@ mod tests {
     pub fn creation() {
         {
             let m = Matrix::new();
-            assert_eq!([[0.;4];4], m.mat);
+            assert_eq!([[0.; 4]; 4], m.mat);
         }
 
         {
             let m = Matrix::identity();
             assert_eq!(
-                [[1., 0., 0., 0.],
-                [0., 1., 0., 0.],
-                [0., 0., 1., 0.],
-                [0., 0., 0., 1.]], 
-                m.mat);
+                [
+                    [1., 0., 0., 0.],
+                    [0., 1., 0., 0.],
+                    [0., 0., 1., 0.],
+                    [0., 0., 0., 1.]
+                ],
+                m.mat
+            );
         }
     }
 
@@ -281,21 +322,27 @@ mod tests {
         {
             let m = Matrix::identity().scalar_mul(3.);
             assert_eq!(
-                [[3., 0., 0., 0.],
-                [0., 3., 0., 0.],
-                [0., 0., 3., 0.],
-                [0., 0., 0., 3.]], 
-                m.mat);
+                [
+                    [3., 0., 0., 0.],
+                    [0., 3., 0., 0.],
+                    [0., 0., 3., 0.],
+                    [0., 0., 0., 3.]
+                ],
+                m.mat
+            );
         }
 
         {
             let m = Matrix::identity().scalar_div(2.);
             assert_eq!(
-                [[0.5, 0., 0., 0.],
-                [0., 0.5, 0., 0.],
-                [0., 0., 0.5, 0.],
-                [0., 0., 0., 0.5]], 
-                m.mat);
+                [
+                    [0.5, 0., 0., 0.],
+                    [0., 0.5, 0., 0.],
+                    [0., 0., 0.5, 0.],
+                    [0., 0., 0., 0.5]
+                ],
+                m.mat
+            );
         }
     }
 
@@ -346,25 +393,25 @@ mod tests {
             let scale = Matrix::scale(2., 3., 4.);
             let inverse = scale.inverse();
             let product = scale * inverse;
-            mat_equal(&Matrix::identity(), &product, 2.*EPSILON);
+            mat_equal(&Matrix::identity(), &product, 2. * EPSILON);
         }
         {
             let mat = Matrix::identity();
             let inverse = mat.inverse();
-            mat_equal(&Matrix::identity(), &inverse, 2.*EPSILON);
+            mat_equal(&Matrix::identity(), &inverse, 2. * EPSILON);
         }
         {
             let translate = Matrix::translate(2., 2., -4.);
             let inverse = translate.inverse();
             let product = inverse * translate;
-            mat_equal(&Matrix::identity(), &product, 2.*EPSILON);
+            mat_equal(&Matrix::identity(), &product, 2. * EPSILON);
         }
         {
             let mut mat = Matrix::identity();
             mat.mat[1][3] = 4.;
             let inverse = mat.inverse();
             let product = inverse * mat;
-            mat_equal(&Matrix::identity(), &product, 2.*EPSILON);
+            mat_equal(&Matrix::identity(), &product, 2. * EPSILON);
         }
         {
             let rotx = Matrix::rotate_x(82.);
@@ -372,7 +419,7 @@ mod tests {
             let product = rotx * inverse;
             println!("{:?}", rotx);
             println!("{:?}", inverse);
-            mat_equal(&Matrix::identity(), &product, 2.*EPSILON);
+            mat_equal(&Matrix::identity(), &product, 2. * EPSILON);
         }
     }
 
@@ -381,20 +428,26 @@ mod tests {
         {
             let m = Matrix::scale(2., 3., 4.);
             assert_eq!(
-                [[2., 0., 0., 0.],
-                [0., 3., 0., 0.],
-                [0., 0., 4., 0.],
-                [0., 0., 0., 1.]], 
-                m.mat);
+                [
+                    [2., 0., 0., 0.],
+                    [0., 3., 0., 0.],
+                    [0., 0., 4., 0.],
+                    [0., 0., 0., 1.]
+                ],
+                m.mat
+            );
         }
         {
             let m = Matrix::translate(2., 3., 4.);
             assert_eq!(
-                [[1., 0., 0., 2.],
-                [0., 1., 0., 3.],
-                [0., 0., 1., 4.],
-                [0., 0., 0., 1.]], 
-                m.mat);
+                [
+                    [1., 0., 0., 2.],
+                    [0., 1., 0., 3.],
+                    [0., 0., 1., 4.],
+                    [0., 0., 0., 1.]
+                ],
+                m.mat
+            );
         }
     }
 
@@ -427,7 +480,7 @@ mod tests {
         {
             let roty = Matrix::rotate_y(90.);
             let r = roty.vec_mul(&v1);
-            assert_within_eps(&Vector4::new(1., 1.,-1., 1.), &r);
+            assert_within_eps(&Vector4::new(1., 1., -1., 1.), &r);
         }
         {
             let rotz = Matrix::rotate_z(90.);
@@ -465,7 +518,7 @@ mod tests {
         {
             let roty = Matrix::rotate_y(90.);
             let r = roty.pt_mul(&p);
-            pt_assert_within_eps(&Point3::new(1., 1.,-1.), &r);
+            pt_assert_within_eps(&Point3::new(1., 1., -1.), &r);
         }
         {
             let rotz = Matrix::rotate_z(90.);
